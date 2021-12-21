4bd6989ff26b2666408568dcd9693e515f066c31
Otavio Rosa <otavio.nunes021@gmail.com>
2021-12-21 17:08:38 -0300

    front-end do perfil feito

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,21 +1,44 @@
 import React, { useState } from 'react';
 import '../stylesheet/pages/Perfil.css';
 
+
+import imagem from '../image/Ellipse 1.png';
+
 import { Link } from 'react-router-dom';
 
 
 
-const Login = (props) => {
+const Perfil = (props) => {
     
+    //Use a variavel "user" para jogar no html
+    //imagem ta ruim de passar
+    const [user, setUser] = useState({img:imagem, foto:"Pedro Costa", tipo:"Moderado", texto:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum ultricies sit nulla etiam sagittis  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum ultricies sit nulla etiam sagittis  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum ultricies sit nulla etiam sagittis  <br />  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum ultricies sit nulla etiam sagittis  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum ultricies sit nulla etiam sagittis  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum ultricies sit nulla etiam sagittis"});
+    
+
+
 
     return (   
         <div>
-            <Link to="/">Aqui mostraremos o perfil do usuario</Link>
+            <div className='imgNome'>
+                <img src={imagem} className='imgpessoa' id='img'/>   
+                <label htmlFor="img" className='nomecorno'> {user.foto} </label>
+                <p className='frase'>O seu perfil de investimento é:</p>
+
+                <label  className='tipo'> {user.tipo} </label>
+
+                <p className='frase texto'>  {user.texto}     </p>
+
+                <input type="submit" value="VER INVESTIMENTOS" id='botao' className='botaoEstilo btnPerfil' />
+
+            </div>
+            <div className='fundo'></div>
             
+
+
         </div>
   
     );
   }
   
-  export default Login;
+  export default Perfil;
   
